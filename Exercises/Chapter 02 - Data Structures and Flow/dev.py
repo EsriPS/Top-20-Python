924ff07f37a3da3e367d9fd3d55584d02334e1d2
dancranford <=>
2024-04-17 07:46:49 -0700

    hitting pause again

--- Exercises/Chapter 02 - Data Structures and Flow/dev.py
+++ Exercises/Chapter 02 - Data Structures and Flow/dev.py
@@ -54,7 +54,7 @@ for number in list_of_numbers:
 import arcpy
 import json
 
-fc = r"C:\Users\dav11274\Desktop\github\Top-20-Python\Exercises\Chapter 02 - ArcPy Basics\Chapter 02 Files\Chapter 02 - Working with Maps.gdb\highways_intersect"
+fc = r"C:\Users\dav11274\Desktop\github\Top-20-Python\Exercises\Chapter 03 - ArcPy Basics\Chapter 03 Files\Chapter 02 - Working with Maps.gdb\highways_intersect"
 
 
 fset = arcpy.FeatureSet(fc)
@@ -69,8 +69,26 @@ for key in json_fc.keys():
 
 
 
-import arcgis
-df = arcgis.features.GeoAccessor.from_featureclass(fc)
+
+import pandas
+import requests
+import json
+import datetime
 
 
-geom = df.SHAPE[0]
+url = "https://data.sfgov.org/resource/wg3w-h783.json"
+
+now = datetime.datetime.now()
+start_date = now - datetime.timedelta(days=7)
+midnight = datetime.time()
+
+# Combine the date and time
+start_day_midnight = datetime.datetime.combine(start_date, midnight)
+formatted_start_day = start_day_midnight.strftime('%Y-%m-%dT%H:%M:%S.%f') 
+formatted_start_day
+
+
+resp = requests.get(url, params={'$where': f"incident_date > '{formatted_start_day}'"})
+
+with open('data.json', 'w') as f:
+    f.write(resp.text)
